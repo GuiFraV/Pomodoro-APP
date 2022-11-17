cee73c1e0efb69c26f6709b36bd43c4907ea7cfb
guillaume.franguiadakis <guillaume.franguiadakis@eemi.com>
2022-11-17 15:34:07 +0100

    add function timer

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //Selectors:
 const optionSelect = document.querySelectorAll('.op');
 const cursor = document.querySelector('.cursor');
-const svg = document.querySelector('svg');
+const svg = document.querySelector('.set');
 const setting = document.querySelector('.settings');
 const opacity = document.querySelector('#opacity');
+const circle = document.querySelector('circle');
+const timerDisplay = document.querySelector('.time');
+const start = document.querySelector('.pause');
 
 //Variables Globales:
 let flag = true;
+let countdown;
 
 //Logiques:
 function select(){
@@ -30,11 +34,70 @@ function modal(){
     changeFlag();
     flag ? settings.style.visibility = 'visible' : settings.style.visibility = 'hidden';
     flag ? opacity.style.visibility = 'visible' : opacity.style.visibility = 'hidden';
+    flag ? circle.style.opacity = '0' : circle.style.opacity = '1';
 }
 
 function changeFlag(){
     flag = !flag;
 }
+
+
+
+function startTimer(){
+
+    if(start.innerHTML == 'PAUSE'){
+
+        start.innerHTML = 'START';
+        
+        clearInterval(countdown);
+
+        
+        
+    }else{
+        
+        start.innerHTML = 'PAUSE';
+        
+        let time = timerDisplay.textContent.split(':');
+
+        let m = parseInt(time[0]);
+
+        let s = parseInt(time[1]);
+
+        let convertM = ((m * 60));
+
+        timer((convertM + s) -1);
+
+    }
+}
+
+function timer(seconds){
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now())/1000);
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+        
+    },1000);
+}
+
+function displayTimeLeft(seconds){
+
+    const minutes = Math.floor(seconds / 60);
+    const reminderSeconds = seconds % 60;
+    const display = `${minutes}:${reminderSeconds < 10 ? '0' : ''}${reminderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+
+    console.log({minutes, reminderSeconds} );
+}
+
 
 //Events:
 optionSelect.forEach(el => {
@@ -42,18 +105,6 @@ optionSelect.forEach(el => {
 });
 svg.addEventListener('click', modal);
 
-//Test:
+start.addEventListener('click', startTimer);
 
-function timer(seconds){
-    const now = Date.now();
-
-    const then = now + seconds * 1000;
-
-    setInterval(() => {
-
-        const secondsLeft = (then - Date.now())/1000;
-        console.log(secondsLeft);
-
-    },1000);
-}
 
